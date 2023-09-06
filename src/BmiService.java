e046a00ae9f97d3c1adb19af0679334b1402fe6a
eleonora <rahovskaaeleonoraf@gmail.com>
2023-09-06 19:22:39 +0300

    FormattingTheCode

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
-    public int calculate(int m , int h) {
-        int hh = (m* 10000)/(h * h);
+    public int calculate(int m, int h) {
+        int hh = (m * 10000) / (h * h);
         return hh;
     }
 }
